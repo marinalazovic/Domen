9b3b15f7dfe281bff7dc777bc64be99a2da93bcc
marinalazovic <77152358+marinalazovic@users.noreply.github.com>
2021-11-13 20:15:47 +0100

    Add files via upload

---
+++ Klijent/FormaKlijent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Klijent
+{
+    public partial class FormaKlijent : Form
+    {
+        Komunikacija k;
+        public FormaKlijent()
+        {
+            InitializeComponent();
+        }
+
+        private void FormaKlijent_Load(object sender, EventArgs e)
+        {
+            k = new Komunikacija();
+            if (k.poveziSeNaSever())
+            {
+                this.Text = "Povezan.";
+            }
+        }
+    }
+}
---
+++ Klijent/Komunikacija.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+using Domen;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Threading;
+
+
+
+
+namespace Klijent
+{
+    public class Komunikacija
+    {
+        TcpClient klijent;
+        NetworkStream tok;
+        BinaryFormatter formater;
+
+        public bool poveziSeNaSever()
+        {
+            try
+            {
+                klijent = new TcpClient("localhost", 20000);
+                tok = klijent.GetStream();
+                formater = new BinaryFormatter();
+                return true;
+
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        public void kraj()
+        {
+            TransferKlasa transfer = new TransferKlasa();
+            transfer.Operacija = Operacije.Kraj;
+            formater.Serialize(tok, transfer);
+        }
+    }
+}
